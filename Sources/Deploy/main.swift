30c3e89b39b5bc64c794791dad01b6157033553b
Patrick Bohrer <pbohrer@us.ibm.com>
2016-11-11 17:50:37 -0600

    trim down and drive off env variables for docker usage

--- Sources/Deploy/main.swift
+++ Sources/Deploy/main.swift
@@ -19,63 +19,38 @@ import Foundation
 import Kitura
 import HeliumLogger
 import LoggerAPI
-import CloudFoundryEnv
 import TodoList
 
 HeliumLogger.use()
 
-let configFile = "cloud_config.json"
 let databaseServiceName = "TodoListCloudantDatabase"
 let databaseName = "todolist"
 
-extension TodoList {
-    public convenience init(withService: Service) {
-        
-        let host: String
-        let username: String?
-        let password: String?
-        let port: UInt16
-        
-        if let credentials = withService.credentials {
-            host = credentials["host"].stringValue
-            username = credentials["username"].stringValue
-            password = credentials["password"].stringValue
-            port = UInt16(credentials["port"].stringValue)!
-        } else {
-            host = "127.0.0.1"
-            username = nil
-            password = nil
-            port = UInt16(5984)
-        }
-        
-        self.init(database: databaseName, host: host, port: port,
-                  username: username, password: password)
-    }
-}
-
 let todos: TodoList
 
-
-do {
-    let service = try getConfiguration(configFile: configFile,
-                                       serviceName: databaseServiceName)
-    todos = TodoList(withService: service)
-    
-} catch {
-    todos = TodoList()
+func getEnvInt(varName:String, defaultValue:Int) -> Int {
+	var value : Int?
+	
+	if let varStringValue = ProcessInfo.processInfo.environment[varName] {
+		value = Int(varStringValue)
+	}
+	return value ?? defaultValue
 }
+
+let host = ProcessInfo.processInfo.environment["CLOUDANT_HOST"] ?? "127.0.0.1"
+let username = ProcessInfo.processInfo.environment["CLOUDANT_USERNAME"]
+let password = ProcessInfo.processInfo.environment["CLOUDANT_PASSWORD"]
+var dbPort = UInt16(getEnvInt(varName: "CLOUDANT_PORT", defaultValue: 5984))
+
+todos = TodoList(database: databaseName, host: host, port: dbPort, username: username, password: password)
 
 
 let controller = TodoListController(backend: todos)
 
-do {
-    let port = try CloudFoundryEnv.getAppEnv().port
-    Log.verbose("Assigned port is \(port)")
-    
-    Kitura.addHTTPServer(onPort: port, with: controller.router)
-    Kitura.run()
+var port = getEnvInt(varName: "PORT", defaultValue: 8090)
+
+Kitura.addHTTPServer(onPort: port, with: controller.router)
+Kitura.run()
     
     
-} catch CloudFoundryEnvError.InvalidValue {
-    Log.error("Oops... something went wrong. Server did not start!")
-}
+
